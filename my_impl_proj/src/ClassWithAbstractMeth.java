5a660720f40d9d2c732bde037559d62b3274eb51
L-533 <alejandrooxl141@gmail.com>
2025-05-26 19:11:54 -0400

    Interfaces and Abstract classes

---
+++ my_impl_proj/src/ClassWithAbstractMeth.java
@@ -0,0 +1,9 @@
+public abstract class ClassWithAbstractMeth {
+    public abstract  String absMeth3(int num);
+    protected abstract boolean absMeth4(String str);
+    abstract float absMeth5(int num, String str);
+
+    public static void main(String[] args) {
+        System.out.println("This is a class with abstract methods");
+    }
+}
