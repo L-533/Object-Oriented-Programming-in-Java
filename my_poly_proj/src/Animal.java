18ff4902dec7304fb164e33b0bd69b9c4b69eb37
L-533 <alejandrooxl141@gmail.com>
2025-06-05 22:57:25 -0400

    setMethod

--- my_poly_proj/src/Animal.java
+++ my_poly_proj/src/Animal.java
@@ -1,13 +1,18 @@
 public class Animal {
     private String name;
+    private String food;
     public Animal(String name) {
         this.name = name;
     }
     public String sound() {
         return null;
     }
+
+    public void setFood(String food){
+        this.food = food;
+    }
     public String toString() {
-        return name.concat(" says ").concat(sound());
+        return name.concat(" says ").concat(sound().concat(" and eats ").concat(food));
     }
 
 }
--- my_poly_proj/src/AnimalFarm.java
+++ my_poly_proj/src/AnimalFarm.java
@@ -21,7 +21,7 @@ public class AnimalFarm {
                     System.out.println("10 animals added already. Cannot add any more animals!");
                     continue;
                 }
-                System.out.println("Which animal do you want to create? \nPress 1 for dog,"+
+                System.out.println("Which animal do you want to create? \nPress 1 for dog,"+ 
                                     "\n2 for cat " +
                                     "\n3 for cow" );
                 String animalChoice = scanner.nextLine();
@@ -29,16 +29,25 @@ public class AnimalFarm {
                     System.out.println("Enter the name of the dog");
                     String dogName = scanner.nextLine();
                     Animal anmlTmp = new Dog(dogName);
+                    System.out.println("Enter what the dog eats");
+                    String dogFood = scanner.nextLine();
+                    anmlTmp.setFood(dogFood);
                     animals[anmlIdx++] = anmlTmp;
                 } else if (animalChoice.equals("2")) {
                     System.out.println("Enter the name of the cat");
                     String catName = scanner.nextLine();
                     Animal anmlTmp = new Cat(catName);
+                    System.out.println("Enter what the cat eats");
+                    String catFood = scanner.nextLine();
+                    anmlTmp.setFood(catFood);
                     animals[anmlIdx++] = anmlTmp;
                 } else if (animalChoice.equals("3")) {
                     System.out.println("Enter the name of the cow");
                     String cowName = scanner.nextLine();
                     Animal anmlTmp = new Cow(cowName);
+                    System.out.println("Enter what the cow eats");
+                    String cowFood = scanner.nextLine();
+                    anmlTmp.setFood(cowFood);
                     animals[anmlIdx++] = anmlTmp;
                 }
             } else {
