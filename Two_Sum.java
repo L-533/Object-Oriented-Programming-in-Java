3ce844c762ad5d65f6ff68eb6dde33d4aff831a4
LuisLinares <98188267+L-533@users.noreply.github.com>
2025-06-14 10:19:13 -0500

    Update Two_Sum.java

--- Two_Sum.java
+++ Two_Sum.java
@@ -21,3 +21,36 @@ class Solution {
     }
     
 }
+
+// Clase que contiene la solución
+class Solution {
+
+    // Comentarios sobre complejidad
+    // Tiempo: O(n) porque recorre el arreglo una vez
+    // Espacio: O(n) por el uso del HashMap
+
+    public int[] twoSum(int[] nums, int target) {
+        // Crear un mapa para guardar valores e índices
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Recorrer el arreglo
+        for (int i = 0; i < nums.length; i++) {
+            int cur = nums[i]; // Valor actual del arreglo
+
+            // Queremos encontrar x tal que cur + x = target
+            // Entonces x = target - cur
+            int x = target - cur;
+
+            // Si x ya está en el mapa, encontramos la pareja
+            if (map.containsKey(x)) {
+                return new int[] { map.get(x), i }; // Devolvemos los índices
+            }
+
+            // Si no, almacenamos el valor actual y su índice
+            map.put(cur, i);
+        }
+
+        // Si no se encuentra ninguna pareja, retorna null
+        return null;
+    }
+}
